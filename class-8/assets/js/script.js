5b59b405fe080ee6d21c115c3c3337945645617b
baxram_kr <124800639+baxram-kr@users.noreply.github.com>
2023-12-07 07:39:56 +0500

    Add files via upload

---
+++ class-8/assets/js/script.js
@@ -0,0 +1,33 @@
+// let dollar = 12250;
+// let tenge = 26;
+// let ruble = 133;
+// let kurs = prompt("Enter kurs : Dollar , Tenge , Ruble");
+// if(kurs == "Dollar"){
+//     d = +prompt("Create Dollar");
+//     alert(dollar*d)
+// }if(kurs == "Ruble"){
+//     r = +prompt("Create Ruble");
+//     alert(ruble*r)
+// }if(kurs == "Tenge"){
+//     t = +prompt("Create Tenge");
+//     alert(tenge*t)
+// }
+
+// let dog = 7;
+// let rabbit = 10;
+// let cat = 8;
+// let animals = prompt("Enter animal : Dog , Cat , Rabbit");
+// if(animals == "Dog"){
+//     y = +prompt("When was your dog born ?");
+//     d=2023-y; 
+//     alert(dog*d)
+// }
+// if(animals == "Cat"){
+//     y = +prompt("When was your cat born ?");
+//     c=2023-y; 
+//     alert(cat*c)
+// }if(animals == "Rabbit"){
+//     y = +prompt("When was your rabbit born ?");
+//     r=2023-y; 
+//     alert(rabbit*r)
+// }
